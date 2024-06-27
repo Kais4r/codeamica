41df67f2878af99707c6a934a31c34d8ee9b37c0
Le Tran Anh Khoi <143178887+Kais4r@users.noreply.github.com>
2024-06-27 03:25:24 -0700

    Add vs code extension and update navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 export default function Home() {
   return (
     <main>
-      <h1 className="text-h1">Codeamica</h1>
-      <h2 className="text-h2">Home page</h2>
+      <h2 className="text-h3">Home page</h2>
 
       <p className="text-base">A regular paragraph</p>
       <p className="text-base">A description paragraph</p>
-      <p className="text-base note">A little note</p>
+      <p className="note text-base">A little note</p>
     </main>
   );
 }
--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -2,18 +2,25 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div className="bg-White border-b-2 border-b-Black">
-      <div className="text-OuterSpace-10">
-        <Link href="/">
-          <p>Home</p>
-        </Link>
-        <Link href="/pages/dashboard">
-          <p>Dashboard</p>
-        </Link>
-        <Link href="/pages/dashboard/settings">
-          <p>Settings</p>
-        </Link>
+    <div className="Parent">
+      <div className="bg-OuterSpace-0">
+        <h1 className="text-h2 text-White">Codeamica</h1>
+      </div>
+
+      <div className="flex border-b-2 border-OuterSpace-40 bg-OuterSpace-80">
+        <Link href="/">Home</Link>
+        <Link href="/pages/dashboard">Dashboard</Link>
+        <Link href="/pages/dashboard/settings">Settings</Link>
       </div>
     </div>
+    // <div className="pl-4 bg-OuterSpace-0">
+    //   <div className="text-White">
+    //     <Link href="/">Home</Link>
+    //     <Link href="/pages/dashboard">Dashboard</Link>
+    //     <p className="bg-OuterSpace-30">
+    //       <Link href="/pages/dashboard/settings">Settings</Link>
+    //     </p>
+    //   </div>
+    // </div>
   );
 }
